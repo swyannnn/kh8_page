e058fd11082a5f0a0716ae21bce36325826eae40
swyannnn <swyannnn@gmail.com>
2021-11-19 23:02:54 +0800

    hah

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('josephtan/', views.josephtan, name='blog-josephtan'),
-    path('mindyliew/', views.mindyliew, name='blog-mindyliew')
+    path('mindyliew/', views.mindyliew, name='blog-mindyliew'),
+    path('test/', views.test, name='blog-test')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 posts = [
     {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
+        'intro': 'Maths Personal Statement (Joseph Tan)',
+        'description': 'Joseph Tan is not in a university yet. Please do note that he hates Python and somehow he is using django.',
+        'content1': 'My childhood is all about numbers and calculations. Born into a traditional Asian family, where our parents expect us to get straight A’s in all of our subjects. I usually spend all my time studying mathematics during the holidays. When I was 9, my mother gave me a book about Carl Friedrich Gauss and his formula to the sum of the natural numbers. Out of curiosity, I opened the book and had a peek. To my surprise, my holiday studies equipped me with the knowledge to understand how his formula works. I was stunned by how beautiful mathematics can be.',
+        'content2': 'In secondary school, I joined an olympiad team. At that time, I made a few friends who are also passionate about mathematics. We would often stay after school discussing olympiad questions or learning new concepts in mathematics. Subsequently, I got a high distinction in a local olympiad competition. I gained huge confidence from this achievement, for this reason, I am motivated to continue my studies and research in mathematics.'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
+        'intro': 'Maths Personal Statement (Joseph Tan)',
+        'description': 'Joseph Tan is not in a university yet. Please do note that he hates Python and somehow he is using django.',
+        'content': 'My childhood is all about numbers and calculations. Born into a traditional Asian family, where our parents expect us to get straight A’s in all of our subjects. I usually spend all my time studying mathematics during the holidays. When I was 9, my mother gave me a book about Carl Friedrich Gauss and his formula to the sum of the natural numbers. Out of curiosity, I opened the book and had a peek. To my surprise, my holiday studies equipped me with the knowledge to understand how his formula works. I was stunned by how beautiful mathematics can be.',
+        'date_posted': 'August 27, 2018'
     }
 ]
 
@@ -21,10 +21,13 @@ def about(request):
     return render (request, 'blog/about.html', {'title': 'About'})
 
 def josephtan(request):
-    context = {
-        'posts': posts
-    }
-    return render (request, 'blog/josephtan.html', context, {'title': 'Joseph Tan'})
+    return render (request, 'blog/josephtan.html', {'title': 'Joseph Tan'})
 
 def mindyliew(request):
     return render (request, 'blog/mindyliew.html', {'title': 'Mindy Liew'})
+
+def test(request):
+    context = {
+        'posts': posts
+    }
+    return render (request, 'blog/test.html', context)  
